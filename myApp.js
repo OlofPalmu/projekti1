1c7876b84c4c18d97e2b674537839e8f5346a602
OlofPalmu <ville.palmu@student.laurea.fi>
2022-10-25 14:40:10 +0300

    muokkauksia

--- myApp.js
+++ myApp.js
@@ -1,2 +1,8 @@
-function newElement() {
-}
+var x = document.getElementById("addCont");
+            x.addEventListener("click", hae);
+            function hae() {
+                var uusi = document.createElement('p');
+                uusi.innerHTML = "Upouusi p-elementti sivulle";
+                uusi.setAttribute('id', 'newData');
+                document.body.appendChild(uusi)
+            }
